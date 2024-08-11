86c9c02fdaee559c1514056dcc0de86435689fbc
Marsel <kms-official@yandex.ru>
2024-08-11 19:46:19 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 #Module 1 Practice 2
+#Variant 1
 summary_of_homework_completed = 12
 summary_of_homework_time = 1.5
 name_of_course = "Python"
 print (name_of_course + " затраченное время - " + str(summary_of_homework_time/summary_of_homework_completed))
 print ("Курс: " + name_of_course + ", всего задач:" + str(summary_of_homework_completed) + ", затрачено часов: " + str(summary_of_homework_time) + ", среднее время выполнения " + str(summary_of_homework_time/summary_of_homework_completed) + " часа.")
+#Variant 2
+print ('Курс: ',name_of_course,', всего задач:',summary_of_homework_completed, ', затрачено часов: ',summary_of_homework_time,', среднее время выполения ',summary_of_homework_time/summary_of_homework_completed,'часа.')
