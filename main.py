12ff149657636a494f7cf2eb1b0edde9d0b9ea8a
Marsel <kms-official@yandex.ru>
2024-08-11 19:27:23 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+#Module 1 Practice 2
+summary_of_homework_completed = 12
+summary_of_homework_time = 1.5
+name_of_course = "Python"
+print (name_of_course + " затраченное время - " + str(summary_of_homework_time/summary_of_homework_completed))
+print ("Курс: " + name_of_course + ", всего задач:" + str(summary_of_homework_completed) + ", затрачено часов: " + str(summary_of_homework_time) + ", среднее время выполнения " + str(summary_of_homework_time/summary_of_homework_completed) + " часа.")
